b1a92037497a6a89591146b8383cbc3a1516f70d
Arlina Design <Arlina-Design@users.noreply.github.com>
2017-09-14 11:48:23 +0700

    Create diginetwall.js

---
+++ diginetwall.js
@@ -0,0 +1,3 @@
+//<![CDATA[
+var mql=window.matchMedia("screen and (min-width: 60em)");mql.matches&&(window.onload=function(){var n=document.getElementById("diginetwall");null==n&&(window.location.href="http://www.idntheme.com"),n.setAttribute("href","http://www.idntheme.com"),n.setAttribute("rel","nofollow"),n.innerHTML="<a href='http://www.idntheme.com/' target='_blank' title='Kreativitas adalah kunci kesuksesan'>Idntheme</a>"});
+//]]>
